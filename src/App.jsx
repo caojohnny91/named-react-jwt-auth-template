d48ac00d5cd9cdfe68683af7085a81c7a298fbcd
jcao91 <jcao91@yahoo.com>
2024-07-08 12:45:52 -0500

    signup form and functions created

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,29 @@
-import { useState } from 'react'
-import { Routes, Route } from 'react-router-dom';
-import NavBar from './components/NavBar/NavBar';
-import './App.css'
-
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import NavBar from "./components/NavBar/NavBar";
+import "./App.css";
+import Landing from "./components/Landing/Landing";
+import Dashboard from "./components/Dashboard/Dashboard";
+import SignupForm from "./components/SignupForm/SignupForm";
 const App = () => {
-  const [user, setUser] = useState(null)
+  // You can experiment with displaying the components by changing the initial state of user from null to something like { username: 'Shawn Doe' } and back to view the different templates.
+  const [user, setUser] = useState(null);
 
   return (
     <>
       <NavBar user={user} />
-      <h1>Hello world!</h1>
-    </>
-  )
-}
+      <Routes>
+        {user ? 
+          <Route path="/" element={<Dashboard user={user} />} />
+         : 
+          <Route path="/" element={<Landing />} />
+        }
 
-export default App
+        <Route path="/signup" element={<SignupForm setUser={setUser} />} />
+        {/* passing setUser as a prop to our SignupForm component */}
+      </Routes>
+    </>
+  );
+};
+
+export default App;
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,7 +1,33 @@
+import { Link } from "react-router-dom";
+
 const NavBar = ({ user }) => {
   return (
     <>
-      <p>Hello Navbar</p>
+      {/* Logged in: Display a greeting and a Sign Out link.
+Not logged in: Display Sign In and Sign up links. */}
+      {user ? (
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="">Sign Out</Link>
+            </li>
+          </ul>
+        </nav>
+      ) : (
+        <nav>
+          <ul>
+            <li>
+              <Link to="/signin">Sign In</Link>
+            </li>
+            <li>
+              <Link to="/signup">Sign Up</Link>
+            </li>
+          </ul>
+        </nav>
+      )}
     </>
   );
 };
