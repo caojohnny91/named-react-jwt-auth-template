8c29ffc7d21eb60fe4c2243de09125a6e15336d6
jcao91 <jcao91@yahoo.com>
2024-07-08 13:01:08 -0500

    sign in form and functionality created

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import "./App.css";
 import Landing from "./components/Landing/Landing";
 import Dashboard from "./components/Dashboard/Dashboard";
 import SignupForm from "./components/SignupForm/SignupForm";
+import SigninForm from "./components/SigninForm/SigninForm"
 const App = () => {
   // You can experiment with displaying the components by changing the initial state of user from null to something like { username: 'Shawn Doe' } and back to view the different templates.
   const [user, setUser] = useState(null);
@@ -13,13 +14,14 @@ const App = () => {
     <>
       <NavBar user={user} />
       <Routes>
-        {user ? 
+        {user ? (
           <Route path="/" element={<Dashboard user={user} />} />
-         : 
+        ) : (
           <Route path="/" element={<Landing />} />
-        }
+        )}
 
         <Route path="/signup" element={<SignupForm setUser={setUser} />} />
+        <Route path="/signin" element={<SigninForm setUser={setUser} />} />
         {/* passing setUser as a prop to our SignupForm component */}
       </Routes>
     </>
